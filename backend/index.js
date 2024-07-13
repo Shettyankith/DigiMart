a433d4697adf8de91b18761318aeffda81516c8d
Ankith Shetty <shettyankith2003@gmail.com>
2024-07-13 08:45:38 +0530

    cors eerror

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const port = 8080;
 
 // Set up CORS
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
